02f618841b7bd9937046c48aa7d0c65f8f5dfb82
Bagrqn <bagrqn.zahariev@abv.bg>
2020-09-20 09:10:32 +0300

    RequestStatusService - Create, GetDefault, GetByCode

---
+++ Services/FMS.Services/Implementations/Request/RequestService.cs
@@ -0,0 +1,34 @@
+﻿using FMS.Data.Models.Request;
+using System;
+
+namespace FMS.Services.Implementations.Request
+{
+    public class RequestService : IRequest
+    {
+        public void Create(string number, int requestTypeID)
+        {
+            if (string.IsNullOrEmpty(number))
+            {
+                throw new InvalidOperationException("Request number can not be null. ");
+            }
+            var request = new Data.Models.Request.Request()
+            {
+                Number = number,
+                DateCreate = DateTime.UtcNow,
+                IsDeleted = false,
+                RequestTypeID = requestTypeID,
+                RequestStatusID = 1
+            };
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NextStatus()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
